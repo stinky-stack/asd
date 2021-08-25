ec7f78436608d3119d4c008f92e7f5d84b2bb498
stinky-stack <89520263+stinky-stack@users.noreply.github.com>
2021-08-25 03:44:27 -0700

    Add files via upload

---
+++ breh.js
@@ -0,0 +1,32 @@
+'use strict'
+
+var target_date = new Date('September, 30, 2021').getTime();
+ 
+
+var days, hours, minutes, seconds;
+ 
+
+var countdown = document.getElementById('countdown');
+ 
+
+setInterval(function () {
+ 
+    
+    var current_date = new Date().getTime();
+    var seconds_left = (target_date - current_date) / 1000;
+ 
+    
+    days = parseInt(seconds_left / 86400);
+    seconds_left = seconds_left % 86400;
+     
+    hours = parseInt(seconds_left / 3600);
+    seconds_left = seconds_left % 3600;
+     
+    minutes = parseInt(seconds_left / 60);
+    seconds = parseInt(seconds_left % 60);
+     
+    
+    countdown.innerHTML = '<span class="days">' + days +  ' <label>Days</label></span> <span class="hours">' + hours + ' <label>Hours</label></span> <span class="minutes">'
+    + minutes + ' <label>Minutes</label></span> <span class="seconds">' + seconds + ' <label>Seconds</label></span>';  
+ 
+}, 1000);
